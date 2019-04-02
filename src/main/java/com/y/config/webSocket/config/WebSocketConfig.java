b1ced207c1d17d5e857450c16aca7d32d752858d
yinshuaibin <805346635@qq.com>
2019-04-02 11:47:26 +0800

    提交完整的websocket点对点, 广播, 接收客户端发送消息(前台默认将用户存入了websocket, 而且websocket打印数据, 没有解决, 很奇怪)

--- src/main/java/com/y/config/webSocket/config/WebSocketConfig.java
+++ src/main/java/com/y/config/webSocket/config/WebSocketConfig.java
@@ -10,11 +10,12 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Slf4j
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Autowired
     private WebSocketInterceptor webSocketInterceptor;
@@ -51,15 +52,14 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         //应用程序以/app为前缀，代理目的地以/topic、/user为前缀
-        registry.enableSimpleBroker("/topic", "/user"); // 这句话表示在topic和user这两个域上可以向客户端发消息
-        registry.setApplicationDestinationPrefixes("/yyy"); // 表示给指定用户一对一的主题前缀是/user
-        registry.setUserDestinationPrefix("/weby"); //表示客户单向服务器端发送时的主题上面需要加 /weby 作为前缀
+        registry.enableSimpleBroker("/topic", "/toUser"); // 这句话表示在topic和weby这两个域上可以向客户端发消息
+        registry.setApplicationDestinationPrefixes("/weby"); //表示客户单向服务器端发送时的主题上面需要加 /weby 作为前缀
+        registry.setUserDestinationPrefix("/toUser"); // 表示给指定用户一对一的主题前缀是/toUser
     }
 
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
         registration.interceptors(connectedChannelInterceptor);
-        super.configureClientInboundChannel(registration);
     }
 
 
--- src/main/java/com/y/task/TaskController/YTaskController.java
+++ src/main/java/com/y/task/TaskController/YTaskController.java
@@ -6,10 +6,14 @@ import com.y.service.UserService;
 import com.y.task.YTaskEngine;
 import com.y.task.YTaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 @RestController
 public class YTaskController {
@@ -34,8 +38,29 @@ public class YTaskController {
         // 下面3行为测试webSocket
         message.setUserId("1");
         message.setDestination("/user");
-        message.setData("ceshi");
+        message.setData("这是一条点对点消息");
         Consumer.addMessage(message);
+
+
+        WebSocketMessage message1 = new WebSocketMessage();
+        message1.setData("这是一条广播消息");
+        message1.setDestination("/topic");
+        Consumer.addMessage(message1);
     }
 
+    /**
+     * 用来接收消息
+     * @param topic
+     * @param headers
+     */
+    @MessageMapping("/stomp")// WebSocketConfig中配置的接收前台消息的前缀
+    public void getMessage(@Header("atytopic") String topic, @Headers Map<String, Object> headers){
+        System.out.println(topic);
+        System.out.println(headers);
+        WebSocketMessage message = new WebSocketMessage();
+        message.setUserId("1");
+        message.setDestination("/user");
+        message.setData("我收到了你发的消息");
+        Consumer.addMessage(message);
+    }
 }
