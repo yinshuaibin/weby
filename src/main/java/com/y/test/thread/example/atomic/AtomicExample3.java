15253feeb8dec09989912bfe07ad452d3d965644
yinshuaibin <805346635@qq.com>
2019-05-13 15:10:40 +0800

    增加多线程部分测试

--- src/main/java/com/y/test/thread/example/atomic/AtomicExample3.java
+++ src/main/java/com/y/test/thread/example/atomic/AtomicExample3.java
@@ -15,6 +15,7 @@ public class AtomicExample3 {
     private static AtomicIntegerFieldUpdater<AtomicExample3> updater =
             AtomicIntegerFieldUpdater.newUpdater(AtomicExample3.class, "count");
 
+    // 对volatile变量写操作时, 强迫线程写操作后刷新主内存中的值, 进行读操作时, 强迫线程从主内存中读取最新的值
     @Getter
     private volatile int count = 100;
 
